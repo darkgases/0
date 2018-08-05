7270578a06092d199e302b842e320246f143c491
darkgases <709135234@qq.com>
2018-08-05 22:57:35 +0800

    01

---
+++ 00.case/wechat-test-页面跳转/JS/wechat-more.js
@@ -0,0 +1,11 @@
+$(function () {
+    $("#community-content-area-button-right1").click(function () {
+        $("#more-menu1").toggle()
+    })
+    $("#community-content-area-button-right2").click(function () {
+        $("#more-menu2").toggle()
+    })
+    $("#community-content-area-button-right3").click(function () {
+        $("#more-menu3").toggle()
+    })
+})
